26d0192ca4479398f1a3d290109f080b4c9871f8
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-01-23 22:24:56 -0800

    fix views, display books table

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-// const db = require('../models');
-// const { Book } = db;
+const db = require('../models');
+const { Book } = db;
 
 router.get('/', async function(req, res, next) {
-  // const books = await db.findAll();
-  res.render('layout');
+  // res.render('layout', {title: 'Hello'});
+  const books = await Book.findAll();
+  // console.log(res.json(books));
+  res.render('index', { books, title: "Books" });
 });
 
 module.exports = router;
